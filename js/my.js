e09df481ceb986f36b0a954feaf0f0116afd2023
Your Name <you@example.com>
2024-01-19 17:02:47 +0800

    @MJ

--- js/my.js
+++ js/my.js
@@ -24,3 +24,76 @@ function toggleDateInput() {
 	dateInput.disabled = creationDateYet.checked;
 }
 
+//===========新增職缺==============
+//選擇薪資條件變動輸入框
+document.addEventListener('DOMContentLoaded', function () {
+    // 監聽 select 的 change 事件
+    document.getElementById('jobSalary').addEventListener('change', function () {
+        // 取得選擇的選項值
+        var selectedOption = this.value;
+
+        // 定義一個通用的函數來處理添加或移除類的邏輯
+		function addClass(classNames, ids) {
+			ids.forEach(function (id) {
+				var element = document.getElementById(id);
+				if (element) {
+					classNames.forEach(function (className) {
+						element.classList.add(className);
+					});
+				}
+			});
+		}
+		function removeClass(classNames, ids) {
+			ids.forEach(function (id) {
+				var element = document.getElementById(id);
+				if (element) {
+					classNames.forEach(function (className) {
+						element.classList.remove(className);
+					});
+				}
+			});
+		}
+
+        // 根據選項值顯示相應的元素
+        switch (selectedOption) {
+            case '時薪':
+            case '日薪':
+                addClass(["choose"], ["dollars"]);
+				removeClass(["choose"], ['dollarsToDollars', 'moreThenDollars', 'negotiable', 'dollarsPerCase']);
+				document.getElementById('optionsRadios1').checked = true; // #optionsRadios1 增加 checked 屬性
+                break;
+            case '月薪':
+				addClass(["choose"], ['dollarsToDollars', 'moreThenDollars', 'negotiable',"dollars"]);
+				removeClass(["choose"], [ 'dollarsPerCase']);
+				document.getElementById('optionsRadios1').checked = true; // #optionsRadios1 增加 checked 屬性
+                break;
+            case '按件計酬':
+				addClass(["choose"], ['dollarsPerCase']);
+				removeClass(["choose"], ['dollarsToDollars', 'moreThenDollars', 'negotiable',"dollars" ]);
+				document.getElementById('optionsRadios5').checked = true; // #optionsRadios1 增加 checked 屬性
+                break;
+            default:
+                break;
+        }
+    });
+});
+//拼湊薪資區間
+function combineInputs() {
+	// 檢查 optionsRadios2 是否被選中
+	var dollarsToDollars = document.getElementById('optionsRadios2').checked;
+
+	if (dollarsToDollars) {
+		// 取得兩個 input 的值
+		var firstValue = document.querySelector('.combineInputs:nth-child(1)').value;
+		var secondValue = document.querySelector('.combineInputs:nth-child(2)').value;
+
+		// 組合兩個值，用 '~' 連接
+		var combinedValue = firstValue + '~' + secondValue;
+
+		// 賦值給 JobSalaryData
+		document.querySelector('[name="JobSalaryData"]').value = combinedValue;
+	}
+}
+
+
+
